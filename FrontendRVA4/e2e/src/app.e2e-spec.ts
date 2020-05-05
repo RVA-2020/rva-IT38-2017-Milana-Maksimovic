f3336c1dee53517e079504a3bd3650c53c8d8fb4
milana998 <milanamaksimovic59@gmail.com>
2020-05-05 14:55:23 +0200

    Initial Commit

--- FrontendRVA4/e2e/src/app.e2e-spec.ts
+++ FrontendRVA4/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('FrontendRVA1 app is running!');
+    expect(page.getTitleText()).toEqual('FrontendRVA4 app is running!');
   });
 
   afterEach(async () => {
--- FrontendRVA4/src/app/app.component.spec.ts
+++ FrontendRVA4/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FrontendRVA1'`, () => {
+  it(`should have as title 'FrontendRVA4'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FrontendRVA1');
+    expect(app.title).toEqual('FrontendRVA4');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FrontendRVA1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('FrontendRVA4 app is running!');
   });
 });
