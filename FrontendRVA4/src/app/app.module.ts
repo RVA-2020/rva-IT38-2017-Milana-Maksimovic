b03f65086942546c72287df1d99eccd0d6061eeb
milana998 <milanamaksimovic59@gmail.com>
2020-05-10 16:14:11 +0200

    Vezbe 8

--- FrontendRVA4/src/app/app.module.ts
+++ FrontendRVA4/src/app/app.module.ts
@@ -1,14 +1,46 @@
+
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ProizvodComponent } from './components/proizvod/proizvod.component';
+import { ProizvodjacComponent } from './components/proizvodjac/proizvodjac.component';
+import { RacunComponent } from './components/racun/racun.component';
+import { StavkaRacunaComponent } from './components/stavka-racuna/stavka-racuna.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { AuthorComponent } from './components/core/author/author.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProizvodComponent,
+    ProizvodjacComponent,
+    RacunComponent,
+    StavkaRacunaComponent,
+    HomeComponent,
+    AboutComponent,
+    AuthorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
